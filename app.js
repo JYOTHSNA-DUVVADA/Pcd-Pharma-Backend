46028965060ebceaf0f9ef5de033d37605342e41
pavan-kotni2005 <kotnipavan8@gmail.com>
2026-07-03 21:34:20 +0530

    Completed

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
     credentials: true
 }));
 app.use(express.json());
